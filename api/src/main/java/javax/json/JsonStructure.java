c8fc84c24427d76d6c5f98b61e9c6c4619b06a61
Jitendra Kotamraju <jitendra.kotamraju@oracle.com>
2013-01-31 16:51:23 -0800

    Minor javadoc edit

--- api/src/main/java/javax/json/JsonStructure.java
+++ api/src/main/java/javax/json/JsonStructure.java
@@ -41,7 +41,7 @@
 package javax.json;
 
 /**
- * Super type for JSON's two structured types ({@link JsonObject object}s
+ * Super type for the two structured types in JSON ({@link JsonObject object}s
  * and {@link JsonArray array}s).
  *
  * @author Jitendra Kotamraju
