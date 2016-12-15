e66c54cf827ec32585a1e252e237183ecbdd6372
Lukas Jungmann <lukas.jungmann@oracle.com>
2016-12-15 15:37:56 +0100

    JSON_PROCESSING_SPEC-81: prepare for JDK9 uptake
    Signed-off-by: Lukas Jungmann <lukas.jungmann@oracle.com>

--- impl/src/main/java/org/glassfish/json/api/BufferPool.java
+++ impl/src/main/java/org/glassfish/json/api/BufferPool.java
@@ -1,7 +1,7 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- * Copyright (c) 2013 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2013-2016 Oracle and/or its affiliates. All rights reserved.
  *
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common Development
@@ -60,6 +60,8 @@ public interface BufferPool {
 
     /**
      * Returns an object back to the pool.
+     *
+     * @param buf object to return back to the pool
      */
     void recycle(char[] buf);
 
