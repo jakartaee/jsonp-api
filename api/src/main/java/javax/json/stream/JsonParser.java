07608edfc55358cd71d93bd4e7c20b86b6f32839
Lukas Jungmann <lukas.jungmann@oracle.com>
2016-12-21 13:28:58 +0100

    small javadoc fix
    Signed-off-by: Lukas Jungmann <lukas.jungmann@oracle.com>

--- api/src/main/java/javax/json/stream/JsonParser.java
+++ api/src/main/java/javax/json/stream/JsonParser.java
@@ -1,7 +1,7 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- * Copyright (c) 2011-2013 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2011-2016 Oracle and/or its affiliates. All rights reserved.
  *
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common Development
@@ -271,8 +271,8 @@ public interface JsonParser extends /*Auto*/Closeable {
      * @throws JsonParsingException if the parser encounters invalid JSON
      * when advancing to next state.
      * @throws java.util.NoSuchElementException if there are no more parsing
-     * @return the event for the next parsing state
      * states.
+     * @return the event for the next parsing state
      */
     Event next();
 
