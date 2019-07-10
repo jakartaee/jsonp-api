ef64055f8eea8b49ef7e42be5926818e861e3387
Dmitry Kornilov <dmitry.kornilov@oracle.com>
2019-07-10 22:14:38 +0200

    JAX-RS renamed to Jakarta RESTful Web Services (#185)

--- demos/jaxrs/src/main/java/org/glassfish/jsondemos/jaxrs/DemoApplication.java
+++ demos/jaxrs/src/main/java/org/glassfish/jsondemos/jaxrs/DemoApplication.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2012, 2017 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2012, 2019 Oracle and/or its affiliates. All rights reserved.
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Distribution License v. 1.0, which is available at
@@ -19,7 +19,7 @@ import java.util.Map;
 import java.util.Set;
 
 /**
- * A JAX-RS Demo Application using JSON API
+ * A Jakarta RESTful Web Services Demo Application
  *
  * @author Jitendra Kotamraju
  */
