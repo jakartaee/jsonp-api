41d3e8e87046ae52381ad24337e5b2ebe40036be
Lukas Jungmann <lukas.jungmann@oracle.com>
2019-07-19 16:11:43 +0200

    add speclicense, remove JSON-P occurences, update readme
    Signed-off-by: Lukas Jungmann <lukas.jungmann@oracle.com>

--- api/src/main/jdk9/module-info.java
+++ api/src/main/jdk9/module-info.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2016, 2017 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2016, 2019 Oracle and/or its affiliates. All rights reserved.
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v. 2.0, which is available at
@@ -14,6 +14,9 @@
  * SPDX-License-Identifier: EPL-2.0 OR GPL-2.0 WITH Classpath-exception-2.0
  */
 
+/**
+ * Jakarta JSON Processing API.
+ */
 module java.json {
     exports javax.json;
     exports javax.json.spi;
