6ea9d1ac0a56090d746e136d615caa546eeb94cc
gurunandan.rao@oracle.com <gurunandan.rao@oracle.com>
2021-12-08 21:16:25 +0530

    jsonp bundle generation.
    Signed-off-by: gurunandan.rao@oracle.com <gurunandan.rao@oracle.com>

---
+++ tck/tck-dist/artifact-install.sh
@@ -0,0 +1,59 @@
+#!/usr/bin/env bash
+
+##script to install the artifact directory contents into a local maven repository
+
+if [[ $1 =~ ^[0-9]+\.[0-9]+\.[0-9]+.*$ ]]; then
+  VERSION="$1"
+else
+  VERSION="2.1.0-SNAPSHOT"
+fi
+
+# Parent pom
+mvn org.apache.maven.plugins:maven-install-plugin:3.0.0-M1:install-file \
+-Dfile=jakarta.json-tck-"$VERSION".pom -DgroupId=jakarta.json \
+-DartifactId=jakarta.json-tck -Dversion="$VERSION" -Dpackaging=pom
+
+# pom
+mvn org.apache.maven.plugins:maven-install-plugin:3.0.0-M1:install-file \
+-Dfile=jakarta.json-tck-common-"$VERSION".pom -DgroupId=jakarta.json \
+-DartifactId=jakarta.json-tck-common -Dversion="$VERSION" -Dpackaging=pom
+
+# jar
+mvn org.apache.maven.plugins:maven-install-plugin:3.0.0-M1:install-file \
+-Dfile=jakarta.json-tck-common-"$VERSION".jar -DgroupId=jakarta.json \
+-DartifactId=jakarta.json-tck-common -Dversion="$VERSION" -Dpackaging=jar
+
+# sources jar
+mvn org.apache.maven.plugins:maven-install-plugin:3.0.0-M1:install-file \
+-Dfile=jakarta.json-tck-common-"$VERSION"-sources.jar -DgroupId=jakarta.json \
+-DartifactId=jakarta.json-tck-common-sources -Dversion="$VERSION" -Dpackaging=jar
+
+# pom
+mvn org.apache.maven.plugins:maven-install-plugin:3.0.0-M1:install-file \
+-Dfile=jakarta.json-tck-tests-"$VERSION".pom -DgroupId=jakarta.json \
+-DartifactId=jakarta.json-tck-tests -Dversion="$VERSION" -Dpackaging=pom
+
+# jar
+mvn org.apache.maven.plugins:maven-install-plugin:3.0.0-M1:install-file \
+-Dfile=jakarta.json-tck-tests-"$VERSION".jar -DgroupId=jakarta.json \
+-DartifactId=jakarta.json-tck-tests -Dversion="$VERSION" -Dpackaging=jar
+
+# sources jar
+mvn org.apache.maven.plugins:maven-install-plugin:3.0.0-M1:install-file \
+-Dfile=jakarta.json-tck-tests-"$VERSION"-sources.jar -DgroupId=jakarta.json \
+-DartifactId=jakarta.json-tck-tests-sources -Dversion="$VERSION" -Dpackaging=jar
+
+# pom
+mvn org.apache.maven.plugins:maven-install-plugin:3.0.0-M1:install-file \
+-Dfile=jakarta.json-tck-tests-plugability-"$VERSION".pom -DgroupId=jakarta.json \
+-DartifactId=jakarta.json-tck-tests-plugability -Dversion="$VERSION" -Dpackaging=pom
+
+# jar
+mvn org.apache.maven.plugins:maven-install-plugin:3.0.0-M1:install-file \
+-Dfile=jakarta.json-tck-tests-plugability-"$VERSION".jar -DgroupId=jakarta.json \
+-DartifactId=jakarta.json-tck-tests-plugability -Dversion="$VERSION" -Dpackaging=jar
+
+# sources jar
+mvn org.apache.maven.plugins:maven-install-plugin:3.0.0-M1:install-file \
+-Dfile=jakarta.json-tck-tests-plugability-"$VERSION"-sources.jar -DgroupId=jakarta.json \
+-DartifactId=jakarta.json-tck-tests-plugability-sources -Dversion="$VERSION" -Dpackaging=jar
