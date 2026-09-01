1031d5542e32a1f83e1663f00d331097c98568d1
Kyle Aure <KyleJAure@gmail.com>
2026-09-01 16:11:09 -0500

    workflow: add performance workflow and persist performance data

--- perf/src/main/java/jakarta/json/jmh/provider/NoopJsonProvider.java
+++ perf/src/main/java/jakarta/json/jmh/provider/NoopJsonProvider.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2026 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2026 Contributors to the Eclipse Foundation
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v. 2.0, which is available at
@@ -13,7 +13,7 @@
  *
  * SPDX-License-Identifier: EPL-2.0 OR GPL-2.0 WITH Classpath-exception-2.0
  */
-package jakarta.json.jmh;
+package jakarta.json.jmh.provider;
 
 import jakarta.json.JsonArray;
 import jakarta.json.JsonArrayBuilder;
